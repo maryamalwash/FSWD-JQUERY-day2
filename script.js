0e7bc2b9ed757eb5b4a7be29159635161b574a9d
Maryam <maryammer@icloud.com>
2018-05-08 10:56:57 +0200

    i have done the basic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-script.js
+$("document").ready(function(){
+	$(".img1").on("click",function(){
+
+	$(".heading2").text("the name is..box")
+    })
+
+	$(".img2").on("click",function(){
+	$(".heading2").text("the name is..santaclaus")
+    })
+
+    $(".img3").on("click",function(){
+	$(".heading2").text("the name is..bells")
+    })
+
+
+});
