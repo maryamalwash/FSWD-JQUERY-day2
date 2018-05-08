f6853371ec80e3727581cd5b80ce875a8d423207
sameh shahin <shahinsameh1000@yahoo.com>
2018-05-08 10:55:09 +0200

    basic 1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-script.js
+$('document').ready(function(){
+	$('.box').on('click',function(){
+		$('.name').remove();
+	$('.result').append('<h3 class=name >box</h3>')
+
+	})
+
+	$('.santa').on('click',function(){
+		$('.name').remove();
+	$('.result').append('<h3 class=name >santa</h3>') 
+	})
+
+	$('.bell').on('click',function(){
+		$('.name').remove();
+	$('.result').append('<h3 class=name >bell</h3>') 
+    })
+
+}); 
